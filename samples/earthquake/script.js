a4a7da3f9b0a8493d097c4e70d0c05bca79ba3e5
Byron Lutz <byronlutz@gmail.com>
2016-10-23 13:45:45 -0700

    Updated sidebar and map

--- samples/earthquake/script.js
+++ samples/earthquake/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9'
   });
+  
+  var listTemplate = Handlebars.compile($('#list-template').html());
 
   var BASE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/';
   var SELECTED_CLASS = 'btn-primary'
@@ -12,6 +14,7 @@ $(document).ready(function() {
     magnitude: '4.5',
     time: 'month'
   }
+  var currentData = {}
   setMagnitude(currentOptions.magnitude);
   setTime(currentOptions.time);
 
@@ -30,10 +33,52 @@ $(document).ready(function() {
     $('[data-eq-magnitude]').removeClass('btn-primary');
     $('[data-eq-magnitude="'+magnitude+'"]').addClass('btn-primary');
     currentOptions.magnitude = magnitude;
+    updateData();
   }
   function setTime(time) {
     $('[data-eq-time]').removeClass('btn-primary');
     $('[data-eq-time="'+time+'"]').addClass('btn-primary');
     currentOptions.time= time;
+    updateData();
+  }
+  
+  function updateData() {
+    var url = BASE_URL + currentOptions.magnitude + '_' + currentOptions.time + '.geojson';
+    $.get(url)
+    .done(function(resp) {
+      if(typeof resp == 'string') {
+        currentData = {error: resp}
+        return;
+      }
+      currentData = resp;
+    }).always(function() {
+      updateList();
+      updateMap();
+    })
+  }
+  
+  function updateList() {
+    var context = {}
+    context.earthquakes = currentData.features.map(function(el) {
+      return {
+        title: el.properties.title
+      }
+    });
+    $('#sidebar-list').html(listTemplate(context));
+  }
+  var markers = []
+  function updateMap() {
+    markers.forEach(function(m) {
+      m.remove();
+    })
+    markers = [];
+    currentData.features.forEach(function(quake) {
+      var el = document.createElement('div');
+      $(el).addClass('marker');
+      var marker = new mapboxgl.Marker(el)
+        .setLngLat([quake.geometry.coordinates[0], quake.geometry.coordinates[1]])
+        .addTo(map);
+       markers.push(marker);
+    });
   }
 });
