4aff619c98e549fb6fdea0f8e1cd04f643107068
Byron Lutz <byronlutz@gmail.com>
2016-10-23 12:24:40 -0700

    Added button interactions

--- samples/earthquake/script.js
+++ samples/earthquake/script.js
@@ -4,17 +4,36 @@ $(document).ready(function() {
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9'
   });
-  
+
   var BASE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/';
+  var SELECTED_CLASS = 'btn-primary'
   
+  var currentOptions = {
+    magnitude: '4.5',
+    time: 'month'
+  }
+  setMagnitude(currentOptions.magnitude);
+  setTime(currentOptions.time);
+
   $('[data-eq-magnitude]').click(function(e) {
     e.preventDefault();
     var magnitude = $(e.target).data('eq-magnitude')
-    console.log(magnitude);
+    setMagnitude(magnitude);
   });
   $('[data-eq-time]').click(function(e) {
     e.preventDefault();
     var time = $(e.target).data('eq-time')
-    console.log(time);
+    setTime(time);
   });
+
+  function setMagnitude(magnitude) {
+    $('[data-eq-magnitude]').removeClass('btn-primary');
+    $('[data-eq-magnitude="'+magnitude+'"]').addClass('btn-primary');
+    currentOptions.magnitude = magnitude;
+  }
+  function setTime(time) {
+    $('[data-eq-time]').removeClass('btn-primary');
+    $('[data-eq-time="'+time+'"]').addClass('btn-primary');
+    currentOptions.time= time;
+  }
 });
