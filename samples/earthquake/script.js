f89d807b180facb1820f61aa594bc2ed5d181553
Byron Lutz <byronlutz@gmail.com>
2016-10-23 12:13:03 -0700

    Added basic JS interactions

--- samples/earthquake/script.js
+++ samples/earthquake/script.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9'
   });
+  
+  var BASE_URL = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/';
+  
+  $('[data-eq-magnitude]').click(function(e) {
+    e.preventDefault();
+    var magnitude = $(e.target).data('eq-magnitude')
+    console.log(magnitude);
+  });
+  $('[data-eq-time]').click(function(e) {
+    e.preventDefault();
+    var time = $(e.target).data('eq-time')
+    console.log(time);
+  });
 });
